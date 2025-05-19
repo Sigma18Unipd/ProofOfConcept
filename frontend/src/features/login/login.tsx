5fb656e249b0c86a9c97e221b98de3bc4b6a816b
Mircodj <43444087+Mircodj@users.noreply.github.com>
2025-05-19 19:03:04 +0200

    feat: Login UI Completed

--- frontend/src/features/login/login.tsx
+++ frontend/src/features/login/login.tsx
@@ -1,9 +1,94 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+ 
+
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+})
+
+
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values);
+}
+
+
 
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
   return (
-    <div>
-      <Button>Login</Button>
+    <div className="flex flex-row min-h-screen justify-center items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your credentials to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Email address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Insert your password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end">
+              <Button type="submit">Login</Button> 
+            </div>
+          </form>
+        </Form>  
+        </CardContent>
+      </Card>
     </div>
   );
 }
