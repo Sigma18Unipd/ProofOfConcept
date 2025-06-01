63dc481a3756ee6dd27b7fb2bb0c76fcf539b10d
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-06-02 00:08:01 +0200

    feat: globally required auth, with optional @public decorator

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
-import { AuthService } from './auth/auth.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   //imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest'), AuthModule, UsersModule],
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ]
 })
 export class AppModule {}
+
