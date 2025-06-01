0d24242bac3e05352059230d68a3a26292caf357
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-06-01 23:18:59 +0200

    feat: authentication (draft) from https://docs.nestjs.com/security/authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest'), AuthModule, UsersModule],
 })
 export class AppModule {}
