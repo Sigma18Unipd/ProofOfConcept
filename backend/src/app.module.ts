f435ae2266b7b28e0fcd4d9a01946e0eb4975943
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-06-01 23:46:54 +0200

    feat: authentication with passport from https://docs.nestjs.com/recipes/passport

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest'), AuthModule, UsersModule],
+  //imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest'), AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController],
 })
 export class AppModule {}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 
 // This should be a real class/interface representing a user entity
