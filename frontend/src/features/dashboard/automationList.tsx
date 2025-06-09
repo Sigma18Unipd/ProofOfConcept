83c347d3307fdb383980e994d360048b368ca25c
Mircodj <43444087+Mircodj@users.noreply.github.com>
2025-06-09 17:44:04 +0200

    removed useless import

--- frontend/src/features/dashboard/automationList.tsx
+++ frontend/src/features/dashboard/automationList.tsx
@@ -1,4 +1,4 @@
-import { Card, CardTitle } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Play } from 'lucide-react';
 
