6460173ddd68566333b2c89a9fa4663fe7c8898a
Mircodj <43444087+Mircodj@users.noreply.github.com>
2025-06-25 16:56:57 +0200

    poc python
    Co-authored-by: Alessandro Bernardello <alessandroberna@users.noreply.github.com>

--- pocUtils/requestTest.py
+++ pocUtils/requestTest.py
@@ -1,10 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import uuid, boto3, json
+
+
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+session = {}
+
+
+
+@app.route('/', methods=['GET'])
+def debug():
+  return json.dumps(list(session.values())), 200
+
 
  
+@app.route('/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  email = data['email']
+  password = data['password']
+  if email == "diomerda" and password == "diomerda":
+    id = str(uuid.uuid4())
+    session[id] = email
+    return jsonify({"authToken": id}), 200
+  return jsonify({"error": "Invalid credentials"}), 401
+
+
+
+@app.route('/checklogin', methods=['POST'])
+def checklogin():
+  authToken = request.cookies.get('authToken')
+  if authToken in session.keys():
+    return 200
+  return 302
+
+
 
 @cross_origin()
 @app.route('/api', methods=['POST'])
@@ -45,5 +78,44 @@ def test():
 
 
 
+
+
+
+# ------------- AI STUFF -------------
+def invoke_agent(prompt):
+    print(f"Invoking agent with prompt: {prompt}")
+    agents_runtime_client = boto3.client("bedrock-agent-runtime", region_name="us-east-1")
+
+    response = agents_runtime_client.invoke_agent(
+        agentId="XKFFWBWHGM",
+        inputText=prompt,
+        agentAliasId="MGRMV9P6PM",
+        sessionId="session-1234567890",
+    )
+
+    completion = ""
+    for event in response.get("completion"):
+        chunk = event["chunk"]
+        completion = completion + chunk["bytes"].decode()
+    return completion
+
+
+
+
+@app.route('/api/prompt', methods=['POST'])
+def prompt():
+    data = request.get_json()
+    prompt = data.get('prompt', '')
+    if not prompt:
+        return jsonify({"error": "Prompt is required"}), 400
+    try:
+        response = invoke_agent(prompt)
+        return jsonify({"response": response}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
+# ------------- RUN -------------
 if __name__ == '__main__':
   app.run(debug=True)
